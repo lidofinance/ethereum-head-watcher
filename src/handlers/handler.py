15101396f0479cbb38598f9407c3c617951165f1
vgorkavenko <vladimir.g@lido.fi>
2023-07-03 11:57:23 +0400

    fix: alerting

--- src/handlers/handler.py
+++ src/handlers/handler.py
@@ -2,10 +2,20 @@ from abc import ABC, abstractmethod
 
 from unsync import unsync
 
+from src.providers.alertmanager.typings import AlertBody
 from src.providers.consensus.typings import BlockHeaderFullResponse
 
 
+# Enough count for different handlers to store in memory
+KEEP_MAX_SENT_ALERTS = 10
+
+
 class WatcherHandler(ABC):
+    sent_alerts = []
+
+    def alert_is_sent(self, current: AlertBody):
+        return str(current.annotations) in [str(s.annotations) for s in self.sent_alerts]
+
     @unsync
     @abstractmethod
     def handle(self, watcher, head: BlockHeaderFullResponse):
@@ -14,3 +24,10 @@ class WatcherHandler(ABC):
         It will be called async and return BlockDetailsResponse.
         """
         pass  # pylint: disable=unnecessary-pass
+
+    def send_alert(self, watcher, alert: AlertBody):
+        if not self.alert_is_sent(alert):
+            watcher.alertmanager.send_alerts([alert])
+            self.sent_alerts.append(alert)
+            if len(self.sent_alerts) > KEEP_MAX_SENT_ALERTS:
+                self.sent_alerts.pop(0)
