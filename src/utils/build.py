3f41892394e97c84ca4a8499a29b83d2fd56cbc2
vgorkavenko <vladimir.g@lido.fi>
2023-08-04 12:48:38 +0400

    fix: add logs

--- src/utils/build.py
+++ src/utils/build.py
@@ -1,5 +1,8 @@
 import json
+import logging
 import os
+
+logger = logging.getLogger(__name__)
 
 UNKNOWN_BUILD_INFO = {"version": "unknown", "branch": "unknown", "commit": "unknown"}
 
@@ -10,7 +13,9 @@ def get_build_info() -> dict:
         with open(path, "r") as f:
             try:
                 build_info = json.load(f)
-            except json.JSONDecodeError:
+            except json.JSONDecodeError as error:
+                logger.warning({"msg": "Failed to load build info file", "error": error})
                 return UNKNOWN_BUILD_INFO
         return build_info
+    logger.info({"msg": "Build info file not found"})
     return UNKNOWN_BUILD_INFO
