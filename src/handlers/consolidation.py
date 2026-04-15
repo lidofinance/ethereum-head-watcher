e2d8f0d03e574d05d5a82eb72f0e66728717c888
Alexander Lukin <alexanderlukin9@gmail.com>
2026-04-16 01:01:13 +0400

    feat: consolidation alerts from our WA
    Add two new critical alerts that are triggered when someone attempts to
    consolidate Lido validators from Lido withdrawal address to non-Lido
    validator and when someone attempts to consolidate non-Lido validators
    from Lido withdrawal address to Lido (or non-Lido) validators.

--- src/handlers/consolidation.py
+++ src/handlers/consolidation.py
@@ -21,37 +21,69 @@ class ConsolidationHandler(WatcherHandler):
             return
 
         slot = head.message.slot
-        withdrawal_address, source_pubkey, target_pubkey = [], [], []
+        our_wa = []
+        our_wa_foreign_source_pubkey = []
+        our_wa_foreign_target_pubkey = []
+        foreign_wa_our_source_pubkey = []
+        foreign_wa_our_target_pubkey = []
         for consolidation in head.message.body.execution_requests.consolidations:
             if consolidation.source_address in watcher.valid_withdrawal_addresses:
-                withdrawal_address.append(consolidation)
-            elif consolidation.source_pubkey in watcher.user_keys:
-                source_pubkey.append(consolidation)
-            elif consolidation.target_pubkey in watcher.user_keys:
-                target_pubkey.append(consolidation)
+                our_wa.append(consolidation)
+
+                if consolidation.source_pubkey not in watcher.user_keys:
+                    our_wa_foreign_source_pubkey.append(consolidation)
+                if consolidation.target_pubkey not in watcher.user_keys:
+                    our_wa_foreign_target_pubkey.append(consolidation)
+            else:
+                if consolidation.source_pubkey in watcher.user_keys:
+                    foreign_wa_our_source_pubkey.append(consolidation)
+                if consolidation.target_pubkey in watcher.user_keys:
+                    foreign_wa_our_target_pubkey.append(consolidation)
             # in the future we should check the type of validator WC:
             # if it is 0x02 and source_address == WCs of source validator - It's donation!
 
-        if withdrawal_address:
-            self._send_withdrawals_address(watcher, slot, withdrawal_address)
-        if source_pubkey:
-            self._send_source_pubkey(watcher, slot, source_pubkey)
-        if target_pubkey:
-            self._send_target_pubkey(watcher, slot, target_pubkey)
+        if our_wa:
+            self._send_withdrawals_address(watcher, slot, our_wa)
+        if our_wa_foreign_source_pubkey:
+            self._send_our_withdrawal_address_foreign_source_pubkey(watcher, slot, our_wa_foreign_source_pubkey)
+        if our_wa_foreign_target_pubkey:
+            self._send_our_withdrawal_address_foreign_target_pubkey(watcher, slot, our_wa_foreign_target_pubkey)
+        if foreign_wa_our_source_pubkey:
+            self._send_foreign_withdrawal_address_our_source_pubkey(watcher, slot, foreign_wa_our_source_pubkey)
+        if foreign_wa_our_target_pubkey:
+            self._send_foreign_withdrawal_address_our_target_pubkey(watcher, slot, foreign_wa_our_target_pubkey)
 
     def _send_withdrawals_address(self, watcher, slot, consolidations: list[ConsolidationRequest]):
         alert = CommonAlert(name="HeadWatcherConsolidationSourceWithdrawalAddress", severity="critical")
         summary = "🚨🚨🚨 Validator consolidation was requested from Withdrawal Vault source address"
         self._send_alert(watcher, slot, alert, summary, consolidations, ADDITIONAL_ALERTMANAGER_LABELS)
 
-    def _send_source_pubkey(self, watcher, slot, consolidations: list[ConsolidationRequest]):
+    def _send_our_withdrawal_address_foreign_source_pubkey(
+        self, watcher, slot, consolidations: list[ConsolidationRequest]
+    ):
+        alert = CommonAlert(name="HeadWatcherConsolidationOurWithdrawalAddressForeignSourcePubkey", severity="critical")
+        summary = "🚨🚨🚨 Validator consolidation was requested for foreign source validator from Withdrawal Vault address"
+        self._send_alert(watcher, slot, alert, summary, consolidations, ADDITIONAL_ALERTMANAGER_LABELS)
+
+    def _send_our_withdrawal_address_foreign_target_pubkey(
+        self, watcher, slot, consolidations: list[ConsolidationRequest]
+    ):
+        alert = CommonAlert(name="HeadWatcherConsolidationOurWithdrawalAddressForeignTargetPubkey", severity="critical")
+        summary = "🚨🚨🚨 Validator consolidation was requested from Withdrawal Vault address to foreign target validator"
+        self._send_alert(watcher, slot, alert, summary, consolidations, ADDITIONAL_ALERTMANAGER_LABELS)
+
+    def _send_foreign_withdrawal_address_our_source_pubkey(
+        self, watcher, slot, consolidations: list[ConsolidationRequest]
+    ):
         alert = CommonAlert(name="HeadWatcherConsolidationUserSourcePubkey", severity="info")
-        summary = "⚠️⚠️⚠️ Consolidation was requested for our validators"
+        summary = "⚠️⚠️⚠️ Consolidation was requested for our validators (not from Withdrawal Vault address)"
         self._send_alert(watcher, slot, alert, summary, consolidations)
 
-    def _send_target_pubkey(self, watcher, slot, consolidations: list[ConsolidationRequest]):
+    def _send_foreign_withdrawal_address_our_target_pubkey(
+        self, watcher, slot, consolidations: list[ConsolidationRequest]
+    ):
         alert = CommonAlert(name="HeadWatcherConsolidationUserTargetPubkey", severity="info")
-        summary = "⚠️⚠️⚠️ Someone attempts to consolidate their validators to our validators"
+        summary = "⚠️⚠️⚠️ Someone attempts to consolidate their validators to our validators (not from Withdrawal Vault address)"
         self._send_alert(watcher, slot, alert, summary, consolidations)
 
     def _send_alert(self, watcher, slot: str, alert: CommonAlert, summary: str,
